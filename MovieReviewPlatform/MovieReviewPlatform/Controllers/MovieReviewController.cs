b500f4e3631e118a41339864ce86b8561031f773
Charles Seguin <132387959+CharlesS42@users.noreply.github.com>
2024-09-12 14:49:18 -0400

    Add files via upload
    initial commit

---
+++ MovieReviewPlatform/MovieReviewPlatform/Controllers/MovieReviewController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieReviewPlatform.Models;
+
+namespace MovieReviewPlatform.Controllers
+{
+    public class MovieReviewController
+    {
+
+        public IActionResult GetReview()
+        {
+            return View(); //Return the view that will display the reviews.
+        }
+
+        public IActionResult AddReview(Movie movie)
+        {
+            //check if movie is valid, most likely to be done in the service rather than here.
+            //Just thought to call the method made in the service here.
+        }
+        {
+
+        }
+    }
+}
